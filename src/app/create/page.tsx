bdd7c503d02fe8b0620a8f6cc4f2bb8fe68a03c8
joey-cheng-codes <j.cheng93@gmail.com>
2023-08-01 18:51:32 -0400

    Center the form and add margin

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,20 +1,22 @@
-export default function Create() {
+import React from "react";
+
+export default function Create(): JSX.Element {
   return (
     <div data-theme='light'>
-      <div className='m-5'>
-        <h1 className='flex flex-col items-center text-4xl'>Add A New Subscription</h1>
+      <div className='m-5 flex flex-col items-center'>
+        <h1 className='flex flex-col items-center text-4xl my-10'>Add A New Subscription</h1>
         <form className="w-full max-w-xl">
           <div className="flex flex-col -mx-3 mb-6">
             {/* Start of Subscription Name */}
             <div className="relative flex w-full flex-wrap items-stretch mb-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                 Subscription Name:
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Name of Subscription" />
             </div>
             {/* Start of Textbox with user's login information */}
             <div className="relative flex w-full flex-wrap items-stretch mb-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                 Subscription Information:
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Username/Email/Password Info" />
@@ -31,21 +33,21 @@ export default function Create() {
             </div>
             {/* Number of Users */}
             <div className="relative flex w-full flex-wrap items-stretch mb-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                 Maximum Number of Users:
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="# of Max Users" />
             </div>
             {/* Create Rules */}
             <div className="relative flex w-full flex-wrap items-stretch mb-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                 Rules of Usage:
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Describe any rules here..." />
             </div>
             {/* Invite Emails */}
             <div className="relative flex w-full flex-wrap items-stretch mb-3">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                 User to Invite:
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Enter Recipient Email Address..." />
