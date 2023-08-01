79de4f9d768d1ebf6cd482a14d51f40da5753a7d
inorle <elinorweissberg@gmail.com>
2023-08-01 19:34:56 -0400

    created login and create account

--- src/app/api/newaccount/route.ts
+++ src/app/api/newaccount/route.ts
@@ -1,15 +1,19 @@
 import type { NextRequest, NextResponse } from 'next/server'
 import db from '../models/dbmodel'
+import bcrypt from 'bcrypt'
 
-export async function POST (request: NextRequest) {
-    const res = await request.json();
-    const values = [res.email, res.password];
+export async function POST(request: NextRequest) {
     try {
-        const createProfile = 'INSERT INTO users (email, password) VALUES ($1, $2) ';
-        const result = await db.query(createProfile, values);
-        return new Response(JSON.stringify(res.email), { status: 200 })
+    const res = await request.json();
+    const saltRounds = 10;
+    const password = await bcrypt.hash(res.password, saltRounds);
+    const values = [res.email, password];
+    const createProfile = 'INSERT INTO users (email, password) VALUES ($1, $2) ';
+    const result = await db.query(createProfile, values);
+    return new Response(JSON.stringify(res.email), { status: 200 })
     }
     catch (err) {
+        console.log(err);
         return new Error;
     }
 
