e200ee87803025b174bb1786355626e6a32c6f44
inorle <elinorweissberg@gmail.com>
2023-08-01 17:08:51 -0400

    added new user

--- src/app/api/models/dbmodel.ts
+++ src/app/api/models/dbmodel.ts
@@ -1,1 +1,15 @@
 const { Pool } = require('pg');
+require("dotenv").config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+  });
+  
+  
+const db = { query: (text:string, params:any) => {
+      console.log('executed query', text);
+      return pool.query(text, params);
+    },
+    pool: pool
+};
+export default db;
--- src/app/api/newaccount/route.ts
+++ src/app/api/newaccount/route.ts
@@ -1,9 +1,17 @@
 import type { NextRequest, NextResponse } from 'next/server'
-import db from '../../../'
+import db from '../models/dbmodel'
 
 export async function POST (request: NextRequest) {
-    const res = await request.json()
+    const res = await request.json();
     console.log(res);
-    return new Response(JSON.stringify( res ), { status: 200 })
+    const values = [res.email, res.password];
+    try {
+        const createProfile = 'INSERT INTO users (email, password) VALUES ($1, $2)';
+        const result = await db.query(createProfile, values);
+        return new Response(JSON.stringify(res.email), { status: 200 })
+    }
+    catch (err) {
+        return new Error;
+    }
 
 }
