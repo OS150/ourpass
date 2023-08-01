17d1749a0894e43e6a0916591db0fe9995191891
inorle <elinorweissberg@gmail.com>
2023-08-01 19:01:56 -0400

    new account

--- src/app/api/newaccount/route.ts
+++ src/app/api/newaccount/route.ts
@@ -3,10 +3,9 @@ import db from '../models/dbmodel'
 
 export async function POST (request: NextRequest) {
     const res = await request.json();
-    console.log(res);
     const values = [res.email, res.password];
     try {
-        const createProfile = 'INSERT INTO users (email, password) VALUES ($1, $2)';
+        const createProfile = 'INSERT INTO users (email, password) VALUES ($1, $2) ';
         const result = await db.query(createProfile, values);
         return new Response(JSON.stringify(res.email), { status: 200 })
     }
