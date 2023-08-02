7b4eb885496eb9e3808ca88f3c1ca22339377ec7
inorle <elinorweissberg@gmail.com>
2023-08-02 14:07:32 -0400

    modified create subscription

--- src/app/api/newsubscription/route.ts
+++ src/app/api/newsubscription/route.ts
@@ -3,27 +3,17 @@ import db from '../models/dbmodel'
 
 
 export async function POST(request: NextRequest) {
-    const {email, text, upload, time, capacity, invite} = await request.json();
+    const {creator_id, name, text, upload, add_info, invite} = await request.json();
     try {
-        //query for id from email
-        const query_id = 'SELECT user_id FROM users where email = $1'
-        const results_user_id = await db.query(query_id, [email]);
-        const creator_id = results_user_id['rows'][0]['user_id'];
         //query for id from invite
         const query_invite_id = 'SELECT user_id FROM users where email = $1';
         const result_invite_id = await db.query(query_invite_id, [invite]);
         const invited_id = result_invite_id['rows'][0]['user_id'];
-
-        //add new rules to rules table and get id back
-        const query_rules = 'INSERT INTO rules (time, capacity) VALUES ($1, $2) RETURNING rules_id';
-        const result_rules = await db.query(query_rules, [time, capacity]);
-        const rules_id = result_rules['rows'][0]['rules_id'];
-
         //add current time
         const date = new Date();
         //add to subscriptions
-        const query_subscriptions = 'INSERT INTO subs (creator_id, text, upload, rules_id, status, date) VALUES ($1, $2, $3, $4, $5, $6) RETURNING invite_id ';
-        const results_subscriptions = await db.query(query_subscriptions, [creator_id, text, upload, rules_id, 'available', date.toLocaleDateString()]);
+        const query_subscriptions = 'INSERT INTO subs (creator_id, name, text, upload, status, date, add_info) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING invite_id ';
+        const results_subscriptions = await db.query(query_subscriptions, [creator_id, name, text, upload, 'available', date.toLocaleDateString(), add_info]);
         const results_id = results_subscriptions['rows'][0]['invite_id'];
         // //add to invite table
         const query_invite = 'INSERT INTO invite (user_id, invite_id) VALUES ($1, $2)'
